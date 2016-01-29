fad4d984a01a7bc1560bd406befda4850c77a18f
jimmynames <jim.me.ryan@gmail.com>
2016-01-29 20:34:23 +0000

    adding files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytplayer', {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.mute();
+    player.playVideo();
+}
+
+
